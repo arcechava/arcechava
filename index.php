ac627ca4c54a5c2952e986e3f245416523f0d89f
arcechava <arcechava@gmail.com>
2016-08-30 18:46:09 -0500

    Quite 2 imagenes persnales y edité la imagen del título contra copyright

--- index.php
+++ index.php
@@ -113,7 +113,6 @@
                                     <li data-target="#carousel-example-generic" data-slide-to="2"></li>
                                     <li data-target="#carousel-example-generic" data-slide-to="3"></li>
                                     <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-                                    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
                                     
                                 </ol>
                             <!-- Slide -->
@@ -134,9 +133,6 @@
                                     <div class="item">
                                         <img src="images/pensar_programar.jpg" class="img-responsive" alt="">
                                     </div>
-                                    <div class="item">
-                                        <img src="images/ing_elvis_arce_chavarria.jpg">
-                                    </div> 
                                 </div>
 
                             </div>
